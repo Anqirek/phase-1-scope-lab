85ab56d3cff7bfe7bdc1c616b9870273ec50530f
Anthony Hill <hillganthony@outlook.com>
2021-09-27 15:01:46 -0400

    commit tree 4ed397f781029622aba695f608f72342be104811

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your solution in this file!
+var customerName = 'bob'
+function upperCaseCustomerName(customerName) {
+customerName.toUpperCase();
+};
+
+
+
+var bestCustomer;
+
+function setBestCustomer(bestCustomer ='not bob'){
+return bestCustomer;
+}
+bestCustomer();
